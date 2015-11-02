9b8a926f8bce33e296995f371400964521b80355
Nan Zhao <nanz@microsoft.com>
2015-11-02 10:45:55 -0800

    redis initial check in

---
+++ CacheLibrary/CacheLibrary/AzureTemplate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheLibrary
+{
+    abstract class AzureTemplate<T>
+    {
+
+        public abstract T GetClient();
+
+        public abstract string Get();
+
+        public abstract void Set(string key, string value);
+    }
+}
---
+++ CacheLibrary/TestCacheClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace TestCacheClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(",abortConnect=false,ssl=true,password=");
+            IDatabase cache=connection.GetDatabase();
+            cache.StringSet("k1", "123123123123123");
+            cache.StringSet("asdf", "zxcvzxcv");
+
+            string r1 = cache.StringGet("k1");
+            string r2 = cache.StringGet("asdf");
+
+            string r3 = null;
+
+        }
+    }
+}
